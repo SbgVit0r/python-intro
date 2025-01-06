2856dbd17a0d2cb9f4d6f54ffb2324a8d66c979c
SbgVit0r <sbeghiv@gmail.com>
2025-01-06 11:47:50 -0300

    ultimo cmmit

--- dia05/api.py
+++ dia05/api.py
@@ -1,1 +1,20 @@
+#%%
 import requests
+
+url = 'https://api.opendota.com/api/heroes'
+
+resposta = requests.get(url)
+# %%
+resposta.status_code
+# %%
+dados = resposta.json()
+# %%
+dados[2]['name']
+# %%
+for i in dados:
+    print(i['name'])
+# %%
+import pandas as pd
+df = pd.DataFrame(dados)
+df.to_csv("heroes_dota.csv", sep=';')
+# %%
